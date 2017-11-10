fd802583e6251efc178d3681ccf461a9b8d891c5
shubhampatwa <shubhampatwa526@gmail.com>
2017-11-10 12:52:40 +0530

    Added basic auth container and component.

---
+++ client/src/components/Base.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Base = () => (
+  <div>
+    <div className="top-bar">
+      <div className="top-bar-left">
+        <Link to="/">React App</Link>
+      </div>
+
+      <div className="top-bar-right">
+        <Link to="/login">Log in</Link>
+        <Link to="/signup">Sign up</Link>
+      </div>
+
+    </div>
+
+  </div>
+);
+
+export default Base;
